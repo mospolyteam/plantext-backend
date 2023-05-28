3434144c419151244a7c139c661146babb755ded
nami <warriorparovoz@gmail.com>
2023-05-28 13:04:57 +0300

    feat: realise Book view and add it to the urls

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Book, Review
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
