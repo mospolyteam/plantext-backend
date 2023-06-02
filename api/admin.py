e4ad15d0ca1f32dd6636e021be43a694adba8c11
nknwns <andrejshaev@gmail.com>
2023-06-03 00:32:31 +0300

    feat(admin): add detailed Book model to admin panel

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import User, Review, Book
 
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'reading_count', 'reviews_count', 'rating')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'author', 'description')
+    readonly_fields = ('rating', 'reviews_count', 'reading_count', 'quotes_count')
+    exclude = ('reading', )
+
+
 admin.site.register(User)
 admin.site.register(Review)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
