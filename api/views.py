3a84fa355ea12f1dd3efa0976b5748ff2f1fbd50
nami <warriorparovoz@gmail.com>
2023-06-12 11:47:37 +0300

    build: mailhog end

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from django.core.mail import send_mail
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 from .models import Book, Review, User
 from .serializers import BookSerializer, ReviewSerializer, UserSerializer
@@ -6,6 +9,20 @@ from .serializers import BookSerializer, ReviewSerializer, UserSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @receiver(post_save)
+    def send_mail_on_create(sender, instance=None, created=False, **kwargs):
+        print(instance, sender)
+        if created:
+            if (sender == Book):
+                send_mail(
+                    "New book",
+                    "We have a new book: " + instance,
+                    "plantext@bookshop.ru",
+                    ["examplemail@mail.com"],
+                    fail_silently=False,
+                )
+
 
 
 class ReviewViewSet(ModelViewSet):
--- plantext/settings.py
+++ plantext/settings.py
@@ -133,3 +133,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'mailhog'
+EMAIL_PORT = 1025
