1a6a1a03fffcfe85be28e6f8194ca68976399bd2
nknwns <andrejshaev@gmail.com>
2023-06-14 13:55:45 +0300

    feat(serializers): add serializers for all models

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
-from .models import Book, Review, User
+from .models import Book, Review, User, Quote, BookRatingRelationship, Article, Writer, WriterRatingRelationship, \
+    Partner
 from rest_framework import serializers
 
 
@@ -21,3 +22,39 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'last_login', 'first_name', 'last_name', 'email', 'photo', 'bio', 'is_active', 'is_staff',
                   'is_superuser']
         read_only_field = ['is_active', 'is_staff', 'is_superuser']
+
+
+class QuoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Quote
+        fields = '__all__'
+
+
+class BookRatingRelationshipSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BookRatingRelationship
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class WriterSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Writer
+        fields = '__all__'
+
+
+class WriterRatingRelationshipSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = WriterRatingRelationship
+        fields = '__all__'
+
+
+class PartnerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Partner
+        fields = '__all__'
