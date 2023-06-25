0393dd85c430b73e555a328fadcc30ad126d3f09
nami <warriorparovoz@gmail.com>
2023-06-25 14:42:31 +0300

    feat(views): add cache use

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from django.core.mail import send_mail
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.core.cache import cache
 
 from .models import Book, Review, User, Writer
 from .serializers import BookSerializer, ReviewSerializer, UserSerializer, WriterSerializer
@@ -10,6 +12,15 @@ from .serializers import BookSerializer, ReviewSerializer, UserSerializer, Write
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def list(self, request):
+        if cache.ttl('books'):
+            queryset = cache.get('books')
+        else:
+            queryset = Book.objects.all()
+            cache.set('books', queryset, 60)
+        serializer = BookSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
 
     @receiver(post_save)
     def send_mail_on_create(sender, instance=None, created=False, **kwargs):
